7f15f354e0873a1d8150b599b889847f64a324f4
Yassen Damyanov <yd@itlabs.bg>
2019-08-14 22:23:40 +0300

    Move usage instructions from source code to the README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Git Goodies
 
+# ps1-4git
+**A git-aware addition to your PS1 prompt**
+
+To make use of this module, download, build and install (you need a C compiler and ``make`` installed on your machine):
+```
+$ git clone --depth=1 https://gitlab.itlabs.bg/open-source/git-goodies.git
+$ cd git-goodies/ps1
+$ make && sudo make install
+```
+
+The ``make install`` step should have added to your ``~/.bashrc`` a line that appends ``ps1-4git`` output to your PS1, similar to what is shown below:
+
+```
+PS1="$PS1\$(git status 2>&1 | git-ps1)"
+```
+
+The ``ps1-4git`` binary outputs such a line when called with ``--4bashrc``, like:
+
+```
+ps1-4git --4bashrc >> ~/.bashrc"  # make sure you do not add references
+```
+
+Check also ``ps1-4git --help``.
--- ps1/git-ps1.c
+++ ps1/git-ps1.c
@@ -1,58 +1,6 @@
 /**
  * Scan 'git status' output (read from stdin) to detect git branch
  * and status of workspace files.
- * Sample 'git status' output:
-
-----cut here--------------
-  On branch bionic
-  Your branch is ahead of 'origin/bionic' by 1 commit.
-  (use "git push" to publish your local commits)
-
-  Changes not staged for commit:
-  (use "git add/rm <file>..." to update what will be committed)
-  (use "git checkout -- <file>..." to discard changes in working directory)
-
-      modified:   ansible.cfg
-      modified:   hosts
-
-  Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-
-      dat-12-plays/
-      roles/
-      scripts/
-      plays/win-baseline.yml
-
-  no changes added to commit (use "git add" and/or "git commit -a")
-----cut here--------------
-
-
-To make use of this module, first compile and install:
-
-$ git clone --depth=1 https://gitlab.itlabs.bg/open-source/git-goodies.git
-$ cd git-goodies/ps1
-$ sudo make install
-
-Then add $(test -d ./.git && git status | git-ps1) to your PS1 environment variable,
-similar to what is shown below:
-
-PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$$(git status 2>&1 | git-ps1) '
-
-
-[1] Example for git status after commit, BEFORE push
-----cut here--------------
-yassen@ubuntu:~/Work/git-goodies$ (br:master) git status
-On branch master
-Your branch is ahead of 'origin/master' by 1 commit.
-  (use "git push" to publish your local commits)
-
-nothing to commit, working tree clean
-----cut here--------------
-
-Error messages when not a git repo:
-  "fatal: not a git repository (or any of the parent directories): .git"
-  "fatal: this operation must be run in a work tree"
-
  */
 
 #include <stdbool.h>
